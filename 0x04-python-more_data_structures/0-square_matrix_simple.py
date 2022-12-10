22cd3a430e74d0547cc6d248c909e1fe31aef338
Nicolas <111075057+NicolasTeeKY@users.noreply.github.com>
2022-12-09 18:56:50 -0800

    squre matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
+
 def squre_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
 
-    for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-
+    new_matrix = [] 
+    for ii in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, ii))
         return (new_matrix)
